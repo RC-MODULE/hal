52001f581e3fc5a447809b0141e7f58c7a6213f5
alexBolotnikov <blcorporation@mail.ru>
2018-03-12 15:15:56 +0300

    templates was changed cfg core 0 and add libc into Makefile

--- app/test-custom/packetDMA/test.cpp
+++ app/test-custom/packetDMA/test.cpp
@@ -7,12 +7,10 @@
 #include <string.h>
 
 void initChain7707(int *buf);
-//extern chain7707 chain;
 
-
-#define MAX_NUM_BUFFERS 16  // Maximum buffers in packet
-#define MAX_BUFFER_SIZE 16	// Maximum size of each buffer 
-#define ALIGN( addr, numInts) (((((unsigned)addr)+numInts-1)>>(numInts-1))<<(numInts-1)) // align "addr" address to boundary of numInts 32-bit words
+#define MAX_NUM_BUFFERS 16 // Maximum buffers in packet
+#define MAX_BUFFER_SIZE 32	// Maximum size of each buffer 
+#define ALIGN( addr, numInts) ((((unsigned)addr)+numInts-1)%numInts*numInts) // align "addr" address to boundary of numInts 32-bit words
 
 int chainBuf[MAX_NUM_BUFFERS*16];
 int main()
@@ -22,23 +20,26 @@ int main()
 	//halOpenDMA((int*)chainBuf); // Создаём пустую таблицу в памяти массива chainBuf
 	
 
-	int numBuffers = 16; // counter of buffers in packet 1
+	//int numBuffers = 1; // counter of buffers in packet 1
 
-	
+	halInitDMA();
+	halEnbExtInt();	
 			
 
-	for (int srcBank = 0; srcBank < 1; srcBank++) {
-		for (int dstBank = 0; dstBank < 3; dstBank++) {
-			nmppsMallocSetRoute16(0xF00 | (dstBank<<4) | dstBank);
+	for (int srcBankIndx = 0; srcBankIndx < 4; srcBankIndx++) {
+		for (int dstBankIndx = 0; dstBankIndx < 4; dstBankIndx++) {
+			nmppsMallocSetRoute16((0xF00) | (dstBankIndx << 4) | srcBankIndx);
 			//nm32s* src = (nm32s*)0x00040000; // (MAX_NUM_BUFFERS*MAX_BUFFER_SIZE * 2);
 			//nm32s* dst = (nm32s*)0x00048000; // (MAX_NUM_BUFFERS*MAX_BUFFER_SIZE * 2);
 			//nmppsMallocSetRoute16(0xF00);
 			nm32s* src = nmppsMalloc_32s(MAX_NUM_BUFFERS*MAX_BUFFER_SIZE * 2);
 			nm32s* dst = nmppsMalloc_32s(MAX_NUM_BUFFERS*MAX_BUFFER_SIZE * 2);
 	
-			nmppsRandUniform_32s(src, MAX_NUM_BUFFERS*MAX_BUFFER_SIZE * 2);
+			//printf("src: %x dst:%x \n", src, dst);
+			//nmppsRandUniform_32s(src, MAX_NUM_BUFFERS*MAX_BUFFER_SIZE * 2);
 			//nmppsSet_32s(dst, 0xcccccccc, MAX_NUM_BUFFERS*MAX_BUFFER_SIZE * 2);
 			memset(dst, 0xcccccccc, MAX_NUM_BUFFERS*MAX_BUFFER_SIZE * 2);
+			memset(src, 0xcccccccc, MAX_NUM_BUFFERS*MAX_BUFFER_SIZE * 2);
 
 			if (src == 0 || dst == 0)
 				return -1;
@@ -49,11 +50,14 @@ int main()
 
 
 			unsigned crcSrc = 0;
-			int bufSize = 16;
+			int bufSize = MAX_BUFFER_SIZE;
 			for (int i = 0; i < MAX_NUM_BUFFERS; i++) {
-				srcAddrList[i] = nmppsAddr_32s((nm32s*)ALIGN(src, 16), i*bufSize * 2);
-				dstAddrList[i] = nmppsAddr_32s((nm32s*)ALIGN(dst, 16), i*bufSize * 2);
-				bufSizeList[i] = bufSize;
+				//srcAddrList[i] = nmppsAddr_32s((nm32s*)ALIGN(src, 16), i*bufSize * 2);
+				//dstAddrList[i] = nmppsAddr_32s((nm32s*)ALIGN(dst, 16), i*bufSize * 2);
+				srcAddrList[i] = nmppsAddr_32s((nm32s*)src, i*bufSize );
+				dstAddrList[i] = nmppsAddr_32s((nm32s*)dst, i*bufSize );
+
+				bufSizeList[i] = bufSize ;
 
 				nmppsCrcAcc_32s(srcAddrList[i], bufSize, &crcSrc);
 			}
@@ -62,22 +66,35 @@ int main()
 			t0 = clock();
 			int err = halInitPacketDMA((void**)srcAddrList, (void**)dstAddrList, (int*)bufSizeList);
 
-			if (err)	return -2;
+			//nmppsCopy_32s(src, dst, MAX_NUM_BUFFERS*MAX_BUFFER_SIZE * 2);
+			//if (err)	return -2;
 
-			int status;
+			int status=1;
 			unsigned time = 0;
-			do {
+			
+			while(status) {
 				halSleep(1);
-				if (time++ > 2000)
+				time++;
+				printf("time %d\n", time);
+				if (time > 2000) {
+					printf("time =%d\n", time);
 					return 333;
+				}
 				status = halStatusDMA();
-			} while (status);
+			}; 
 			t1 = clock();
 
 			unsigned crcDst = 0;
 			for (int i = 0; i < MAX_NUM_BUFFERS; i++) {
 				nmppsCrcAcc_32s(dstAddrList[i], bufSize, &crcDst);
 			}
+			printf("%x %x dt=%d\n", crcSrc, crcDst, t1-t0 );
+
+//		crcDst = 0;
+//		crcSrc = 0;
+		crcSrc = nmppsCrc_32s(src, MAX_NUM_BUFFERS*MAX_BUFFER_SIZE * 2);
+		crcDst = nmppsCrc_32s(dst, MAX_NUM_BUFFERS*MAX_BUFFER_SIZE * 2);
+//
 			if (crcSrc - crcDst)
 				return 9;
 			
