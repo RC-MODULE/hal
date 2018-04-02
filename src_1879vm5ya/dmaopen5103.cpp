651fdfc8a7750af58860c9894cd347ca5a267748
svet <svet@module.ru>
2018-04-02 20:26:22 +0300

    5103 ++

---
+++ src_1879vm5ya/dmaopen5103.cpp
@@ -0,0 +1,13 @@
+extern "C"{
+	
+
+int nonfirst, chain[10], matrixcnt, packetcnt;
+int ofdmainit;
+void dmaopen5103()
+{ nonfirst=0; chain[9]=0;
+  matrixcnt=0;
+  packetcnt=0;
+  ofdmainit=0;
+}
+
+}
