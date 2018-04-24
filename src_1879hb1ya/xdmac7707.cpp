1671c29a4fd9f110de1d886a6aa135bf7d979d2d
svet <svet@module.ru>
2018-04-24 18:54:52 +0300

    7707

--- src_1879hb1ya/xdmac7707.cpp
+++ src_1879hb1ya/xdmac7707.cpp
@@ -1,51 +1,66 @@
 #include "chain7707.h"
+#ifndef xdmac7707tools
+#define xdmac7707tools
+extern SpecDMA7707 chain_7707;
+extern int chainBuf[];
+extern "C"
+{
+  typedef int(*DmaCallback)();
+  int xdmacChain( int* TBL );
+  int xdmacChainN( int* TBL, int channel );
+  int xdmacinitn( int dst, int src, int size32, int channel);	
+  int xdmacinit( int dst, int src, int size32);	
+}
 
-extern unsigned int first7707;
-extern chain7707 chain;
 int add2Chain(int dst,int src,int size);
+SpecDMA7707* halInitDMA7707 (void** src, void** dst,  int* size32, void* specBuffer)
+{   int *q,n,*asrc,*adst,*sz;
+      openDma(specBuffer);
+      asrc=(int*)src;  adst=(int*)dst;	sz=size32;
+  for (;;) 
+  {  n=*sz++;
+     if (n) add2Chain((*adst++)<<2,(*asrc++)<<2,n<<2); else break;
+  }
+   q=chain_7707.root;
+   return (SpecDMA7707*)chain_7707.root;
+}
 
-extern "C" {
-
-    int xdmacChain( int* TBL );
-
-    int  halInitDoubleDMA(void*  src0, void*  src1, void* dst0,   void* dst1, int intSize0, int intSize1)
-    {
-         int *q,k; 
-        add2Chain(((int)dst0)<<2,((int)src0)<<2,intSize0<<2);
-        add2Chain(((int)dst1)<<2,((int)src1)<<2, intSize1<<2);
-        q=chain.root;
-        k=xdmacChain( q );
-        return k;
-    }
-
-    int  halInitMatrixDMA(void*  src,  int  width,  int  height, int srcStride32,  void* dst, int dstStride32, void *DmaCallback, int channel)
-    {
-        int *q,k,i,srcrow,dstrow,asrc,adst,sz;
-        srcrow=srcStride32<<2;   // байтовый размер
-        dstrow=dstStride32<<2; // байтовый размер	
-        sz=width<<2; // байтовый размер
-        asrc=(int)src;  adst=(int)dst;	
-         for (i=0; i<height;i++) 
-        {  
-            add2Chain(adst,asrc,sz);
-            asrc+=srcrow;  adst+=dstrow;
-        }
-        q=chain.root;
-        k=xdmacChain( q );
-        return k;
-    }	
-
-    int  halInitPacketDMA(void** src,  void** dst,  int* size32)
-        { 
-            int *q,k,n,*asrc,*adst,*sz;
-            asrc=(int*)src;  adst=(int*)dst;	sz=size32;
-            for (;;) {  
-                n=*sz++;
-                if (n) add2Chain((*adst++)<<2,(*asrc++)<<2,n<<2); else break;
-            }
-            q=chain.root;
-            k=xdmacChain( q );
-           return k;
-    }	
-
-};
+int halInitSingleDMA(void*  src,  void*  dst,  int  size32,  DmaCallback* func, int channel=0)
+{  
+	return xdmacinitn(((int)dst)<<2, ((int)src)<<2, size32<<2,channel);
+} 
+int  halInitDoubleDMA(void*  src0, void*  src1, void* dst0,   void* dst1, int intSize0, int intSize1)
+{ int *q,k; 
+   add2Chain(((int)dst0)<<2,((int)src0)<<2,intSize0<<2);
+   add2Chain(((int)dst1)<<2,((int)src1)<<2, intSize1<<2);
+   q=chain_7707.root;
+   k=xdmacChain( q );
+   return k;
+}
+int  halInitMatrixDMA(void*  src,  int  width,  int  height, int srcStride32,  void* dst, int dstStride32, void *DmaCallback, int channel)
+{ int *q,k,i,srcrow,dstrow,asrc,adst,sz;
+   srcrow=srcStride32<<2;   // байтовый размер
+   dstrow=dstStride32<<2; // байтовый размер	
+   sz=width<<2; // байтовый размер
+   asrc=(int)src;  adst=(int)dst;	
+  for (i=0; i<height;i++) 
+  {  add2Chain(adst,asrc,sz);
+      asrc+=srcrow;  adst+=dstrow;
+  }
+   q=chain_7707.root;
+   k=xdmacChainN( q, channel);
+   return k;
+}	
+int  halInitPacketDMA(void** src,  void** dst,  int* size32)
+{ int *q,k,n,*asrc,*adst,*sz;
+   q=(int*) halInitDMA7707 (src, dst, size32,  chainBuf);
+   asrc=(int*)src;  adst=(int*)dst;	sz=size32;
+  for (;;) 
+  {  n=*sz++;
+     if (n) add2Chain((*adst++)<<2,(*asrc++)<<2,n<<2); else break;
+  }
+   q=chain_7707.root;
+   k=xdmacChain( q );
+   return k;
+}
+#endif
