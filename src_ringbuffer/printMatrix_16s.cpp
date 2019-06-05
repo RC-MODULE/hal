4deec49193da0e468ad55961321bdeeb6c12de19
Sergey Mushkaev <mushkaev@module.ru>
2019-06-05 19:25:11 +0300

    nmpss-

--- src_ringbuffer/printMatrix_16s.cpp
+++ src_ringbuffer/printMatrix_16s.cpp
@@ -8,7 +8,7 @@ extern "C" void printMatrix_16s(char* text, nm16s* matrix, int height,int width)
 	printf("%s [%s]\n",text,address);
 	for (int y=0,i=0; y<height; y++){
 		for(int x=0; x<width; x++,i++){
-			int val=nmppsGet_16s(matrix,i);
+			int val=halGet_16s(matrix,i);
 			printf("%4d ",val);
 		}
 		printf("\r\n");
--- src_ringbuffer/printMatrix_16x.cpp
+++ src_ringbuffer/printMatrix_16x.cpp
@@ -8,7 +8,7 @@ extern "C" void printMatrix_16x(char* text, nm16s* matrix, int height,int width)
 	printf("%s [%s]\n",text,address);
 	for (int y=0,i=0; y<height; y++){
 		for(int x=0; x<width; x++,i++){
-			unsigned short val=nmppsGet_16s(matrix,i);
+			unsigned short val=halGet_16s(matrix,i);
 			printf("%4x ",val);
 		}
 		printf("\r\n");
