8bd599e4245affac351cc5c139a3fdbc778a6343
Sergey Mushkaev <mushkaev@module.ru>
2019-07-05 15:11:00 +0300

    12101 profile ++

---
+++ examples/profiler-standalone/profile-tbl-out/make_mc12101_nmpu1/proflist_generated.asm
@@ -0,0 +1,32 @@
+//****************************************************************************************
+// This is automaticly generated asm-file by profiler-gcc.py
+//****************************************************************************************
+import from "nmprofiler.mlb";
+begin ".text_nmprofiler"
+PROFILE_BEGIN( 16 );
+
+	FUNCTION( _strncpy ,          "_strncpy        ");//  2002c46a  [ .text ]
+	NONCFUNC( Mul32 ,             "Mul32           ");//  2002c3d6  [ .text ]
+	NONCFUNC( IDiv32 ,            "IDiv32          ");//  2002c424  [ .text ]
+	FUNCTION( _mycos ,            "_mycos          ");//  2002c042  [ .text ]
+	FUNCTION( _mysin ,            "_mysin          ");//  2002c080  [ .text ]
+	NONCFUNC( UDiv32 ,            "UDiv32          ");//  2002c412  [ .text ]
+	FUNCTION( _vsprintf ,         "_vsprintf       ");//  2002c820  [ .text ]
+	NONCFUNC( UMod32 ,            "UMod32          ");//  2002e102  [ .text ]
+	FUNCTION( UMod64 ,            "UMod64          ");//  2002e164  [ .text ]
+	FUNCTION( UDiv64 ,            "UDiv64          ");//  2002e27a  [ .text ]
+	FUNCTION( RShift64 ,          "RShift64        ");//  2002e398  [ .text ]
+	FUNCTION( LShift64 ,          "LShift64        ");//  2002e4b4  [ .text ]
+	NONCFUNC( IMod32 ,            "IMod32          ");//  2002e12e  [ .text ]
+	FUNCTION( _malloc ,           "_malloc         ");//  2002f0f2  [ .text ]
+	FUNCTION( _free ,             "_free           ");//  2002f9c0  [ .text ]
+	FUNCTION( _abort ,            "_abort          ");//  2002fb10  [ .text ]
+	FUNCTION( _memcpy ,           "_memcpy         ");//  2002fb62  [ .text ]
+	FUNCTION( _memset ,           "_memset         ");//  2002fb26  [ .text ]
+	NONCFUNC( RShift32 ,          "RShift32        ");//  2002fb90  [ .text ]
+	NONCFUNC( ARShift32 ,         "ARShift32       ");//  2002fc1c  [ .text ]
+	NONCFUNC( LShift32 ,          "LShift32        ");//  200303d8  [ .text ]
+	FUNCTION( _memmove ,          "_memmove        ");//  20030464  [ .text ]
+
+PROFILE_END();
+end ".text_nmprofiler";
---
+++ examples/profiler-standalone/profile-tbl-out/make_mc12101_nmpu1_gcc/proflist_generated.asm
@@ -0,0 +1,35 @@
+//****************************************************************************************
+// This is automaticly generated asm-file by profiler-gcc.py
+//****************************************************************************************
+import from "nmprofiler.mlb";
+begin ".text_nmprofiler"
+PROFILE_BEGIN( 16 );
+
+	FUNCTION( _sum ,              "_sum            ");//  000000002000004e  [ .text ]
+	FUNCTION( _mycos ,            "_mycos          ");//  000000002000007e  [ .text ]
+	FUNCTION( _mysin ,            "_mysin          ");//  00000000200000a8  [ .text ]
+	NONCFUNC( ARShift32 ,         "ARShift32       ");//  000000002000066a  [ .text ]
+	NONCFUNC( UDiv32 ,            "UDiv32          ");//  0000000020000718  [ .text ]
+	NONCFUNC( IDiv32 ,            "IDiv32          ");//  000000002000072a  [ .text ]
+	FUNCTION( IDiv64 ,            "IDiv64          ");//  0000000020000798  [ .text ]
+	FUNCTION( UDiv64 ,            "UDiv64          ");//  00000000200007fe  [ .text ]
+	NONCFUNC( LShift32 ,          "LShift32        ");//  000000002000081c  [ .text ]
+	FUNCTION( LShift64 ,          "LShift64        ");//  00000000200008a8  [ .text ]
+	NONCFUNC( UMod32 ,            "UMod32          ");//  00000000200009c4  [ .text ]
+	NONCFUNC( IMod32 ,            "IMod32          ");//  00000000200009f0  [ .text ]
+	FUNCTION( UMod64 ,            "UMod64          ");//  0000000020000a26  [ .text ]
+	FUNCTION( IMod64 ,            "IMod64          ");//  0000000020000a62  [ .text ]
+	NONCFUNC( Mul32 ,             "Mul32           ");//  0000000020000aae  [ .text ]
+	NONCFUNC( RShift32 ,          "RShift32        ");//  0000000020000ac8  [ .text ]
+	FUNCTION( RShift64 ,          "RShift64        ");//  0000000020000b54  [ .text ]
+	NONCFUNC( DAdd ,              "DAdd            ");//  0000000020000c70  [ .text ]
+	NONCFUNC( DCmp ,              "DCmp            ");//  0000000020000c78  [ .text ]
+	NONCFUNC( DDiv ,              "DDiv            ");//  0000000020000c82  [ .text ]
+	FUNCTION( _fmod_yx ,          "_fmod_yx        ");//  0000000020000ca2  [ .text ]
+	NONCFUNC( DMul ,              "DMul            ");//  0000000020000cca  [ .text ]
+	NONCFUNC( ConvDtoI32 ,        "ConvDtoI32      ");//  0000000020000cd2  [ .text ]
+	NONCFUNC( ConvI32toD ,        "ConvI32toD      ");//  0000000020000ce8  [ .text ]
+	NONCFUNC( ConvU32toD ,        "ConvU32toD      ");//  0000000020000cf0  [ .text ]
+
+PROFILE_END();
+end ".text_nmprofiler";
