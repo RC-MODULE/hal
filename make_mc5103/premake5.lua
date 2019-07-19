9150b1623dfca1d390f5d744dfb4396d6186c40e
Andrey Brodyazhenko <brodyazhenko@gmail.com>
2019-07-19 19:39:26 +0300

    123

--- make_mc5103/premake5.lua
+++ make_mc5103/premake5.lua
@@ -5,7 +5,7 @@ solution "hal-mc5103"
    	configurations { "Debug", "Release" }
 
 	-- x86  library with printf support  ---------------------------------	 
-	project "hal-mc5103-x86"
+	project "hal-mc5103"
       		kind "StaticLib"
 		includedirs { "../include","$(MC5103)/libload","../src_host_io"}
  		targetdir ("../lib")	  
@@ -15,38 +15,32 @@ solution "hal-mc5103"
 				"../include/*.h",
 				"../src_host_io/*.*"}
 	  
-		configuration "Debug"
-			targetsuffix ("d")
+		configuration {"Debug","Win32"}
+			targetsuffix ("-x86d")
+			architecture "x32"
 			defines { "DEBUG","NM6405"}
 			symbols  "On" 
 		
-		configuration "Release"
-			
+		configuration {"Release","Win32"}
+			targetsuffix ("-x86")
+			architecture "x32"
 			defines { "NDEBUG","NM6405"}
          	symbols  "Off" 
 		
-	-- x86  library without printf support  ---------------------------------	 
-	project "hal-mc5103silent-x86"
-      		kind "StaticLib"
-		includedirs { "../include","$(MC5103)/libload","../src_host"}
-		targetdir ("../lib")	  
-	
-	      	files { 	"../src_mc5103_host/*.*",
-				"../src_x86/*.*",
-				"../src_ringbuffer/*.*",
-				"../include/*.h"}
-		
-		configuration "Debug"
-			targetsuffix ("d")
-			defines { "DEBUG","SILENT","NM6405"}
+		configuration {"Debug","x64"}
+			targetsuffix ("-x64d")
+			architecture "x64"
+			defines { "DEBUG","NM6405"}
 			symbols  "On" 
-      
-		configuration "Release"
-			defines { "NDEBUG","SILENT","NM6405"}
+		
+		configuration {"Release","x64"}
+			targetsuffix ("-x64")
+			architecture "x64"
+			defines { "NDEBUG","NM6405"}
 			symbols  "Off" 
 		 
 	-- NeuroMatrix project ---------------------------------		
-	project "hal-mc5103"
+	project "hal-mc5103-nmc3"
      		kind "Makefile"
 		includedirs { "../include","$(MC5103)/libload"}
       		files { "../src_1879vm5ya/*.*",
