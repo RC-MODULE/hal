3f55186a1480c03f3c71e80243a22ea2c801e97b
Sergey Mushkaev <mushkaev@module.ru>
2019-07-22 14:48:27 +0300

    profile

--- examples/profiler/host-target-hostprint2tbl/make_host_mc5103/proflist_generated.asm
+++ examples/profiler/host-target-hostprint2tbl/make_host_mc5103/proflist_generated.asm
@@ -1,41 +1,33 @@
-
 //****************************************************************************************
-// This is automaticly generated asm-file by generate_proflist.bat (generate_proflist.tcl )
-// Use this script to parse map-file to extract functions from abs-file 
-// and to generate initial asm-template for profiling.
-// It is recommended to remove  all unnecessary functions to make profiling more precise.
+// This is automaticly generated asm-file by profiler-gcc.py
 //****************************************************************************************
-import from nmprofiler.mlb;
+import from "nmprofiler.mlb";
 begin ".text_nmprofiler"
-PROFILE_BEGIN(16);
+PROFILE_BEGIN( 16 );
 
-
-
-	// place here macros with function name for each function you wish to profile like:
-	// PROFILE_FUNC(_myfunc,"myfunc");
-
-	PROFILE_FUNC(_halSleep,"_halSleep");
-	PROFILE_NONSTD_FUNC(Mul32,"Mul32");
-	PROFILE_FUNC(_func0,"_func0");
-	PROFILE_FUNC(_func1,"_func1");
-	PROFILE_FUNC(halSleep,"halSleep");
-	PROFILE_FUNC(_malloc,"_malloc");
-	PROFILE_FUNC(_free,"_free");
-	PROFILE_NONSTD_FUNC(UMod32,"UMod32");
-	PROFILE_NONSTD_FUNC(UDiv32,"UDiv32");
-	PROFILE_FUNC(UMod64,"UMod64");
-	PROFILE_FUNC(UDiv64,"UDiv64");
-	PROFILE_FUNC(RShift64,"RShift64");
-	PROFILE_FUNC(LShift64,"LShift64");
-	PROFILE_NONSTD_FUNC(IDiv32,"IDiv32");
-	PROFILE_NONSTD_FUNC(IMod32,"IMod32");
-	PROFILE_FUNC(_memcpy,"_memcpy");
-	PROFILE_FUNC(_memset,"_memset");
-	PROFILE_NONSTD_FUNC(RShift32,"RShift32");
-	PROFILE_NONSTD_FUNC(ARShift32,"ARShift32");
-	PROFILE_NONSTD_FUNC(LShift32,"LShift32");
-	PROFILE_FUNC(_memmove,"_memmove");
+	FUNCTION( _halSleep ,         "_halSleep       ");//  000004c4  [ .text_hal ]
+	NONCFUNC( Mul32 ,             "Mul32           ");//  000005fa  [ .text ]
+	FUNCTION( _func0 ,            "_func0          ");//  000004d0  [ .text ]
+	FUNCTION( _func1 ,            "_func1          ");//  00000510  [ .text ]
+	FUNCTION( _vsprintf ,         "_vsprintf       ");//  00001190  [ .text ]
+	FUNCTION( halSleep ,          "halSleep        ");//  000004ac  [ .text_hal ]
+	FUNCTION( _malloc ,           "_malloc         ");//  00002cba  [ .text ]
+	FUNCTION( _free ,             "_free           ");//  000035d0  [ .text ]
+	NONCFUNC( UMod32 ,            "UMod32          ");//  00003726  [ .text ]
+	NONCFUNC( UDiv32 ,            "UDiv32          ");//  000037aa  [ .text ]
+	FUNCTION( UMod64 ,            "UMod64          ");//  00003802  [ .text ]
+	FUNCTION( UDiv64 ,            "UDiv64          ");//  00003918  [ .text ]
+	FUNCTION( RShift64 ,          "RShift64        ");//  00003a36  [ .text ]
+	FUNCTION( LShift64 ,          "LShift64        ");//  00003b52  [ .text ]
+	NONCFUNC( IDiv32 ,            "IDiv32          ");//  000037bc  [ .text ]
+	NONCFUNC( IMod32 ,            "IMod32          ");//  00003752  [ .text ]
+	FUNCTION( _memcpy ,           "_memcpy         ");//  00003caa  [ .text ]
+	FUNCTION( _memset ,           "_memset         ");//  00003c6e  [ .text ]
+	NONCFUNC( RShift32 ,          "RShift32        ");//  00003cd8  [ .text ]
+	NONCFUNC( ARShift32 ,         "ARShift32       ");//  00003d64  [ .text ]
+	FUNCTION( _abort ,            "_abort          ");//  00004520  [ .text ]
+	NONCFUNC( LShift32 ,          "LShift32        ");//  00004536  [ .text ]
+	FUNCTION( _memmove ,          "_memmove        ");//  000045c2  [ .text ]
 
 PROFILE_END();
 end ".text_nmprofiler";
-
