6c9041763b20bf4c2e2e04de95f7dd853704c8f0
Sergey Mushkaev <mushkaev@module.ru>
2020-06-29 16:32:04 +0300

    ./ instead /. in makefile

--- examples/profiler-standalone/profile-tbl-out/make_mc12101_nmpu0/proflist_generated.asm
+++ examples/profiler-standalone/profile-tbl-out/make_mc12101_nmpu0/proflist_generated.asm
@@ -5,28 +5,6 @@ import from "nmprofiler.mlb";
 begin ".text_nmprofiler"
 PROFILE_BEGIN( 16 );
 
-	FUNCTION( _strncpy ,          "_strncpy        ");//  0002847c  [ .text ]
-	NONCFUNC( Mul32 ,             "Mul32           ");//  000283e8  [ .text ]
-	NONCFUNC( IDiv32 ,            "IDiv32          ");//  00028436  [ .text ]
-	FUNCTION( _mycos ,            "_mycos          ");//  00028054  [ .text ]
-	FUNCTION( _mysin ,            "_mysin          ");//  00028092  [ .text ]
-	NONCFUNC( UDiv32 ,            "UDiv32          ");//  00028424  [ .text ]
-	FUNCTION( _vsprintf ,         "_vsprintf       ");//  00028832  [ .text ]
-	NONCFUNC( UMod32 ,            "UMod32          ");//  0002a114  [ .text ]
-	FUNCTION( UMod64 ,            "UMod64          ");//  0002a176  [ .text ]
-	FUNCTION( UDiv64 ,            "UDiv64          ");//  0002a28c  [ .text ]
-	FUNCTION( RShift64 ,          "RShift64        ");//  0002a3aa  [ .text ]
-	FUNCTION( LShift64 ,          "LShift64        ");//  0002a4c6  [ .text ]
-	NONCFUNC( IMod32 ,            "IMod32          ");//  0002a140  [ .text ]
-	FUNCTION( _malloc ,           "_malloc         ");//  0002b104  [ .text ]
-	FUNCTION( _free ,             "_free           ");//  0002b9d2  [ .text ]
-	FUNCTION( _abort ,            "_abort          ");//  0002bb22  [ .text ]
-	FUNCTION( _memcpy ,           "_memcpy         ");//  0002bb74  [ .text ]
-	FUNCTION( _memset ,           "_memset         ");//  0002bb38  [ .text ]
-	NONCFUNC( RShift32 ,          "RShift32        ");//  0002bba2  [ .text ]
-	NONCFUNC( ARShift32 ,         "ARShift32       ");//  0002bc2e  [ .text ]
-	NONCFUNC( LShift32 ,          "LShift32        ");//  0002c3ea  [ .text ]
-	FUNCTION( _memmove ,          "_memmove        ");//  0002c476  [ .text ]
 
 PROFILE_END();
 end ".text_nmprofiler";
--- examples/profiler/host-target-hostprint2tbl/make_host_mc5103/proflist_generated.asm
+++ examples/profiler/host-target-hostprint2tbl/make_host_mc5103/proflist_generated.asm
@@ -5,29 +5,6 @@ import from "nmprofiler.mlb";
 begin ".text_nmprofiler"
 PROFILE_BEGIN( 16 );
 
-	FUNCTION( _halSleep ,         "_halSleep       ");//  000004c4  [ .text_hal ]
-	NONCFUNC( Mul32 ,             "Mul32           ");//  000005fa  [ .text ]
-	FUNCTION( _func0 ,            "_func0          ");//  000004d0  [ .text ]
-	FUNCTION( _func1 ,            "_func1          ");//  00000510  [ .text ]
-	FUNCTION( _vsprintf ,         "_vsprintf       ");//  00001190  [ .text ]
-	FUNCTION( halSleep ,          "halSleep        ");//  000004ac  [ .text_hal ]
-	FUNCTION( _malloc ,           "_malloc         ");//  00002cba  [ .text ]
-	FUNCTION( _free ,             "_free           ");//  000035d0  [ .text ]
-	NONCFUNC( UMod32 ,            "UMod32          ");//  00003726  [ .text ]
-	NONCFUNC( UDiv32 ,            "UDiv32          ");//  000037aa  [ .text ]
-	FUNCTION( UMod64 ,            "UMod64          ");//  00003802  [ .text ]
-	FUNCTION( UDiv64 ,            "UDiv64          ");//  00003918  [ .text ]
-	FUNCTION( RShift64 ,          "RShift64        ");//  00003a36  [ .text ]
-	FUNCTION( LShift64 ,          "LShift64        ");//  00003b52  [ .text ]
-	NONCFUNC( IDiv32 ,            "IDiv32          ");//  000037bc  [ .text ]
-	NONCFUNC( IMod32 ,            "IMod32          ");//  00003752  [ .text ]
-	FUNCTION( _memcpy ,           "_memcpy         ");//  00003caa  [ .text ]
-	FUNCTION( _memset ,           "_memset         ");//  00003c6e  [ .text ]
-	NONCFUNC( RShift32 ,          "RShift32        ");//  00003cd8  [ .text ]
-	NONCFUNC( ARShift32 ,         "ARShift32       ");//  00003d64  [ .text ]
-	FUNCTION( _abort ,            "_abort          ");//  00004520  [ .text ]
-	NONCFUNC( LShift32 ,          "LShift32        ");//  00004536  [ .text ]
-	FUNCTION( _memmove ,          "_memmove        ");//  000045c2  [ .text ]
 
 PROFILE_END();
 end ".text_nmprofiler";
